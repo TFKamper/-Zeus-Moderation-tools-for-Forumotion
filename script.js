45ca77dda5e801176b0f23f4d65b6cda59cfafcf
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-08-21 18:45:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ $(function() {
                 $(".sceditor-button-staff").removeClass("disabled");
             });
             
-            if(_userdata.user_level > 0) $(".sceditor-group:last-child").before('<div class="sceditor-group"><a class="sceditor-button sceditor-button-staff" title="Mesaje de moderare"><div unselectable="on">Mesaje de moderare</div></a><div class="mod_box" style="display: none;"><ul class="mod_groups" id="mod_box_i">' + list + '<li class="copyright_e"> © Created by Zeus - All right reserved</li></div></div></div>');
+            if(_userdata.user_level == 1 0 && _userdata.user_level == 2) $(".sceditor-group:last-child").before('<div class="sceditor-group"><a class="sceditor-button sceditor-button-staff" title="Mesaje de moderare"><div unselectable="on">Mesaje de moderare</div></a><div class="mod_box" style="display: none;"><ul class="mod_groups" id="mod_box_i">' + list + '<li class="copyright_e"> © Created by Zeus - All right reserved</li></div></div></div>');
         });
     });
 });
